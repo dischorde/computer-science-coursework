7be2cb3cf488cde8eec4d7d0b5595c41d5772a58
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-09-18 16:39:13 -0700

    Add Calculator brain and tie UI to edges

--- STANFORD-CS193P/Calculator/Calculator/ViewController.swift
+++ STANFORD-CS193P/Calculator/Calculator/ViewController.swift
@@ -11,15 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else {
             display!.text = digit
             userIsInTheMiddleOfTyping = true
@@ -27,17 +27,27 @@ class ViewController: UIViewController {
         
     }
     
-    
-    @IBAction func performOperation(_ sender: UIButton) {
-        if let mathematicalOperation = sender.currentTitle {
-            switch mathematicalOperation {
-            case "π":
-                display!.text = "3.1415926"
-            default:
-                break
-            }
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
         }
     }
     
+    private var brain: CalculatorBrain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
 }
-
