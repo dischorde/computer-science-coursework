e7ff0085755d2a52fa81aac98ed5ac8852c0282b
Katarina Rossi <katarina.n.rossi@gmail.com>
2018-05-18 16:35:11 -0700

    Set up concentration as from lecture code demos

---
+++ STANFORD-CS193P/Concentration/Concentration/Concentration.swift
@@ -0,0 +1,43 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Katarina Rossi on 5/18/18.
+//  Copyright © 2018 Katarina Rossi. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: shuffle the cards
+    }
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+}
